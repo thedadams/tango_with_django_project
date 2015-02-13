65af182a3fdb55b6ac3464a42fa725e43288101d
thedadams <donald.g.adams@me.com>
2015-02-12 21:14:11 -0700

    Finished Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -204,7 +204,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 
 
